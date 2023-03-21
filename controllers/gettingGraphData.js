a082cf22bc609e6bc993593323d6bb653d3dfc39
elmotasembelah <79071279+elmotasembelah@users.noreply.github.com>
2023-03-21 19:45:33 +0200

    testing connecting to gettingGraphData

--- controllers/gettingGraphData.js
+++ controllers/gettingGraphData.js
@@ -1,38 +1,42 @@
 const { startCounting } = require("./countingPosts");
 const { StatusCodes } = require("http-status-codes");
 
-const startDisplayGraph = async (req, res) => {
-    const { industryfilter } = req.headers;
+// const startDisplayGraph = async (req, res) => {
+//     const { industryfilter } = req.headers;
 
-    const { countedColors, colorsNamesAndHexValues } = await startCounting(
-        req,
-        res,
-        industryfilter
-    );
+//     const { countedColors, colorsNamesAndHexValues } = await startCounting(
+//         req,
+//         res,
+//         industryfilter
+//     );
 
-    const histogramColors = prepareHistogramColors(
-        countedColors,
-        colorsNamesAndHexValues
-    );
-    const countedColorsValues = Object.values(countedColors);
-    const countedColorsNames = Object.keys(countedColors);
-    res.status(StatusCodes.OK).json({
-        countedColorsNames,
-        countedColorsValues,
-        histogramColors,
-    });
-    // displayChart(datasets, industryName);
-};
+//     const histogramColors = prepareHistogramColors(
+//         countedColors,
+//         colorsNamesAndHexValues
+//     );
+//     const countedColorsValues = Object.values(countedColors);
+//     const countedColorsNames = Object.keys(countedColors);
+//     res.status(StatusCodes.OK).json({
+//         countedColorsNames,
+//         countedColorsValues,
+//         histogramColors,
+//     });
+//     // displayChart(datasets, industryName);
+// };
 
-const prepareHistogramColors = (countedColors, colorsNamesAndHexValues) => {
-    let histogramColors = [];
-    const countedColorsNames = Object.keys(countedColors);
-    countedColorsNames.forEach((colorName) => {
-        colorsNamesAndHexValues.forEach((color) => {
-            if (color.name === colorName) histogramColors.push(color.hexValue);
-        });
-    });
-    return histogramColors;
+// const prepareHistogramColors = (countedColors, colorsNamesAndHexValues) => {
+//     let histogramColors = [];
+//     const countedColorsNames = Object.keys(countedColors);
+//     countedColorsNames.forEach((colorName) => {
+//         colorsNamesAndHexValues.forEach((color) => {
+//             if (color.name === colorName) histogramColors.push(color.hexValue);
+//         });
+//     });
+//     return histogramColors;
+// };
+
+const startDisplayGraph = (req, res) => {
+    res.send("connected to gettingGraphData");
 };
 
 module.exports = { startDisplayGraph };
