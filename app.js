cc3c6408fc797747c0ab63283f872043cdb68172
elmotasembelah <79071279+elmotasembelah@users.noreply.github.com>
2023-03-21 18:57:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
+// const express = require("express");
+// const app = express();
+
+// app.use((req, res) => {
+//     res.send("simple server connected ");
+// });
+
+// const startServer = () => {
+//     try {
+//         app.listen(3000, () => {
+//             console.log("listening on port 3000");
+//         });
+//     } catch (err) {
+//         console.log(err);
+//     }
+// };
+
+// startServer();
+
 const express = require("express");
 const app = express();
-
-app.use((req, res) => {
-    res.send("simple server connected ");
+app.all("/", (req, res) => {
+    console.log("Just got a request!");
+    res.send("Yo!");
 });
-
-const startServer = () => {
-    try {
-        app.listen(3000, () => {
-            console.log("listening on port 3000");
-        });
-    } catch (err) {
-        console.log(err);
-    }
-};
-
-startServer();
+app.listen(process.env.PORT || 3000);
