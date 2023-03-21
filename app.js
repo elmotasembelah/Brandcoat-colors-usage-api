0f8bb8c02717086b8dc79b25868f6ee564cc5354
elmotasembelah <79071279+elmotasembelah@users.noreply.github.com>
2023-03-21 17:58:34 +0200

    reverted to a simple server

--- app.js
+++ app.js
@@ -1,44 +1,17 @@
-require("dotenv");
-require("express-async-errors");
-// const colorUsageGraphRouter = require("./routes/color_usage_graph.js");
-const helmet = require("helmet");
-const cors = require("cors");
-const xss = require("xss-clean");
-const rateLimiter = require("express-rate-limit");
 const express = require("express");
 const app = express();
 
-app.use(express.json());
-app.set("trust proxy", 1);
-app.use(
-    rateLimiter({
-        windowMs: (15 * 60) ^ 1000,
-        max: 100,
-    })
-);
-app.use(helmet());
-app.use(cors());
-app.use(xss());
-
-app.use("/api/", (req, res) => {
-    res.send("connected");
+app.use((req, res) => {
+    res.send("simple server connected");
 });
-
-// app.use("/api/colorusagegraph", colorUsageGraphRouter);
-
-app.use("/", (req, res) => res.send("connected"));
-
-app.use((req, res) => res.status(404).send("Route does not exist"));
-
-const port = process.env.PORT | 3000;
 
 const startServer = () => {
     try {
-        app.listen(port, () => {
-            console.log(`server is listening on port: ${port}`);
+        app.listen(3000, () => {
+            console.log("listening on port 3000");
         });
-    } catch (error) {
-        console.log(error);
+    } catch (err) {
+        console.log(err);
     }
 };
 
