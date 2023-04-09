06f3547af17f1dc94635e4d58319ad36de354cb6
elmotasembelah <79071279+elmotasembelah@users.noreply.github.com>
2023-04-09 19:51:20 +0200

    Update countingPosts.js
    added a template for the colors counted to make their order static and made a reset function for that template

--- controllers/countingPosts.js
+++ controllers/countingPosts.js
@@ -1,17 +1,36 @@
 const { getColorUsageGraphData } = require("./gettingColorUsageGraphData");
 
-let countedColors = {};
+let countedColors = {
+    Red: 0,
+    Orange: 0,
+    Yellow: 0,
+    Lime: 0,
+    Green: 0,
+    Olive: 0,
+    Seafoam: 0,
+    Teal: 0,
+    "Sky Blue": 0,
+    Blue: 0,
+    Violet: 0,
+    Purple: 0,
+    Pink: 0,
+    Coral: 0,
+    Brown: 0,
+    Black: 0,
+    Grayscale: 0,
+};
 
 const startCounting = async (req, res, industryFilter) => {
     let { postsColorsAndIndustryNames, colorsNamesAndHexValues } =
         await getColorUsageGraphData();
-    countedColors = {};
+    ResetCountedColors();
     if (industryFilter !== "All industries") {
         postsColorsAndIndustryNames = filterThenCountPosts(
             postsColorsAndIndustryNames,
             industryFilter
         );
     }
+
     countingPosts(postsColorsAndIndustryNames);
     return { countedColors, colorsNamesAndHexValues };
 };
@@ -27,7 +46,7 @@ const filterThenCountPosts = (postsColorsAndIndustryNames, filter) => {
     return filteredPosts;
 };
 
-// loops dynamicly over every Post sent to it to start cointing colors and saving it in countedColors object
+// loops dynamicly over every Post sent to it to start counting colors and saving it in countedColors object
 const countingPosts = (postsColorsAndIndustryNames) => {
     postsColorsAndIndustryNames.forEach((post) => {
         post.colors.forEach((color) => {
@@ -35,6 +54,13 @@ const countingPosts = (postsColorsAndIndustryNames) => {
                 ? countedColors[color]++
                 : (countedColors[color] = 1);
         });
+    });
+};
+
+const ResetCountedColors = () => {
+    colors = Object.keys(countedColors);
+    colors.forEach((color) => {
+        countedColors[color] = 0;
     });
 };
 
