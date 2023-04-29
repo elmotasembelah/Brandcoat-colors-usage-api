23670dd9028faf3d603be620e9a1d45e1f6bf575
elmotasembelah <79071279+elmotasembelah@users.noreply.github.com>
2023-04-29 14:39:26 +0300

    refactored the code
    removed testing console.log

--- controllers/logoChangeFrequencyChart/getAndPrepareLogoChangeFrequencyChartData.js
+++ controllers/logoChangeFrequencyChart/getAndPrepareLogoChangeFrequencyChartData.js
@@ -11,8 +11,6 @@ const {
 const getAndPrePareLogoChangeFrequencyChartData = async (industryFilter) => {
     let postsCollectionData = await getPostsCollectionData();
 
-    console.log(postsCollectionData.length);
-
     let industriesCollectionData = await getIndustriesCollectionData();
 
     if (industryFilter !== "All industries") {
@@ -23,14 +21,11 @@ const getAndPrePareLogoChangeFrequencyChartData = async (industryFilter) => {
         );
     }
 
-    console.log(postsCollectionData.length);
-
     const postsOldDesignDateAndNewDesignData =
         getPostsOldDesignDateAndNewDesignDate(postsCollectionData, {
             lastPartInDateFormat: "month",
         });
 
-    console.log(postsOldDesignDateAndNewDesignData.length);
     return postsOldDesignDateAndNewDesignData;
 };
 
